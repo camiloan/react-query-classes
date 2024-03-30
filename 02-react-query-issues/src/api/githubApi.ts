df1569ec90f8625b63664d03d553e082dc1d80e7
Camilo Andres Bolaños Andrade <camiloa.andresc@hotmail.com>
2024-03-29 23:49:59 -0500

    Finished Issues

--- 02-react-query-issues/src/api/githubApi.ts
+++ 02-react-query-issues/src/api/githubApi.ts
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {},
+  headers: {
+    Authorization:
+      'Bearer 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',
+  },
 });
